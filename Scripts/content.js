4b43deae6823a949ae8becf29da8ad97e78dc408
Daniel Fulbrecht <13141050+fulbrecht@users.noreply.github.com>
2023-04-16 21:36:43 -0400

    added a mouseenter and mouseleave eventlistener to selected element

--- Scripts/content.js
+++ Scripts/content.js
@@ -4,47 +4,66 @@ let glitchCount = 0
 const style = document.createElement('style')
 
 style.innerHTML = `
-  @keyframes blinking {
+    @keyframes blinking {
     0% { opacity: 1; }
-    50% { opacity: 0.5; }
+    50% { opacity: 0.1; }
     100% { opacity: 1; }
-  }
-`
-
-document.head.appendChild(style);
-
-function addMutationObserver() {
-    const observer = new MutationObserver(function (mutations) {
-        mutations.forEach(function (mutation) {
-            const elements = mutation.target.getElementsByTagName('*');
-            let visibleElements = []
-            // loop through all the elements and check if they are visible
-            for (let i = 0; i < elements.length; i++) {
-                const element = elements[i];
-                if (isVisible(element)) {
-                visibleElements.push(element);
-                }
-            }
-
-            const randomIndex = Math.floor(Math.random() * visibleElements.length)
-            const randomElement = visibleElements[randomIndex]
-            glitch(randomElement)
-        })
-      })
-      observer.observe(document.body, { subtree: true, childList: true })
-}
-
-function glitch(node) {
-    if (glitchCount === 0){
-        node.style.backgroundColor = 'green'
-        node.style.animation = 'blinking 1s infinite'
-        glitchCount = 1
     }
-}
-
-// Helper functions
+    `
+    
+    document.head.appendChild(style);
+    
+    function addMutationObserver() {
+        const observer = new MutationObserver(function (mutations) {
+            mutations.forEach(function (mutation) {
+                const elements = mutation.target.getElementsByTagName('*');
+                let visibleElements = []
+                // loop through all the elements and check if they are visible
+                for (let i = 0; i < elements.length; i++) {
+                    const element = elements[i];
+                    if (isVisible(element) && hasText(element)) {
+                        visibleElements.push(element);
+                    }
+                }
+                
+                const randomIndex = Math.floor(Math.random() * visibleElements.length)
+                const randomElement = visibleElements[randomIndex]
+                glitch(randomElement)
+            })
+        })
+        observer.observe(document.body, { subtree: true, childList: true })
+    }
+    
+    function glitch(node) {
+        if (glitchCount === 0){
+            const oldHTML = node.innerHTML
+            const oldStyle  = node.style
+            node.style.backgroundColor = 'green'
+            node.style.animation = 'blinking 1s infinite'
+            node.addEventListener('mouseenter', () => {
+                node.style.backgroundColor = 'red'
+                node.textContent = 'hello world'
+              });
+              node.addEventListener('mouseleave', () => {
+                node.style = oldStyle
+                // node.style.animation = ''
+                node.innerHTML = oldHTML
+              });
+            glitchCount = 1
+    
+        }
+    }
+    
+    // Helper functions
 function isVisible(e){
     if (e.offsetHeight > 0 && e.offsetWidth > 0){
+        return true
+    }
+    return false
+}
+
+function hasText(e){
+    if (e.textContent){
         return true
     }
     return false
